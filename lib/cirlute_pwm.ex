ce0dbb9b1c4cdb95e8713ac7c91ef8e69c66dd05
Cocoa <r.xu.1@research.gla.ac.uk>
2022-04-04 22:09:45 +0100

    add min_pwm/0 and max_pwm/0

--- lib/cirlute_pwm.ex
+++ lib/cirlute_pwm.ex
@@ -4,4 +4,8 @@ defmodule Cirlute.PWM do
   @callback set_frequency(term(), number()) :: {:ok, term()} | {:error, term()}
 
   @callback set_pwm(term(), non_neg_integer(), non_neg_integer()) :: :ok | {:error, term()}
+
+  @callback min_pwm() :: non_neg_integer()
+
+  @callback max_pwm() :: non_neg_integer()
 end
